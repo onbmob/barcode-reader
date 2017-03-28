631ca22c8cb39ed2b6a102e36b042e5a3270f67e
Билык Олег <onbmob@mail.ru>
2017-03-28 12:26:39 +0300

    ignore

--- app/build/generated/source/r/debug/com/google/android/gms/samples/vision/barcodereader/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/samples/vision/barcodereader/R.java
@@ -3043,9 +3043,10 @@ containing a value of this type.
     }
     public static final class raw {
         public static final int explosion=0x7f070000;
-        public static final int shot=0x7f070001;
-        public static final int task=0x7f070002;
-        public static final int task2=0x7f070003;
+        public static final int route2=0x7f070001;
+        public static final int shot=0x7f070002;
+        public static final int task=0x7f070003;
+        public static final int task2=0x7f070004;
     }
     public static final class string {
         public static final int abc_action_bar_home_description=0x7f080000;
